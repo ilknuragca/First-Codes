0bf580fe450d89f9a121f75211feeb9ffbf48fea
ilknuragca <93740795+ilknuragca@users.noreply.github.com>
2021-11-08 11:28:57 +0300

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,26 @@
+package day01try;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+	 
+		//double convert to int
+		double data=100.325;
+		int convert=(int)data;
+		System.out.println(convert);//100
+
+		
+		//float convert to short
+		float f=15224.45f;
+		short s=(short)f;
+		System.out.println(s);//15224
+		//short convert to byte
+		byte value=(byte)s;
+		System.out.println(value);//120
+		
+		
+		
+		
+	}
+
+}

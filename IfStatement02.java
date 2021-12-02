f1434956b095d17c2b13b62951167bccd2001f0e
ilknuragca <93740795+ilknuragca@users.noreply.github.com>
2021-12-02 22:49:52 +0300

    Update IfStatement02.java

--- IfStatement02.java
+++ IfStatement02.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class IfStatement02 {
 
-	//Note: ýf you want to compare the values of 2 strings use "equals()", do not use "=="
-	// "equals()" check just the values of the strings but "==" checks the values and addresses of the string
+	
 	
 	public static void main(String[] args) {
 		//Ask user to enter any day name , then print if it is "week day" or "weekend day"
@@ -14,7 +13,7 @@ public class IfStatement02 {
         System.out.println("Enter a day name..");
         String dayName=scan.next();
         
-    /*    if(dayName.equals("Monday")  )       // Don't use == for string  
+    /*    if(dayName.equals("Monday")  )         
         {
          System.out.println("Week day");	
         }
@@ -60,53 +59,11 @@ public class IfStatement02 {
 	      {
 	    	  System.out.println("Weekend day");
 	      }
-	      
-	      
-	  /* if(dayName.equalsIgnoreCase("Monday") || dayName.equalsIgnoreCase("Tuesday") || dayName.equalsIgnoreCase("Wednesday")||dayName.equalsIgnoreCase("Thursday") || dayName.equalsIgnoreCase("Friday"))      
-	          // "||" one of them true so conditional is true 
-	            {
-	            System.out.println("Week day");	
-	           }
-	   	      if(dayName.equalsIgnoreCase("Saturday") || dayName.equalsIgnoreCase("Sunday"))
-	   	      {
-	   	    	  System.out.println("Weekend day");
-	   	      }
-	   	      
-	    */   // equalsIgnoreCase() checks in the string by ignoring cases. when u use ignorecase methods uppercase or lowercase are same 
-	      
-	      
-	      
- /*   if(!dayName.equals("Monday") || !dayName.equals("Tuesday") || !dayName.equals("Wednesday")|| !dayName.equals("Thursday") || !dayName.equals("Friday"))      
-	      {
-	    	  System.out.println("You don't write day");
-	      }
-	      
-	//!!!it's very long
-	 * */
-	      
-	      else {       ///so use "else"
+	
+	      else {      
 	    	  System.out.println("Hey user, I told you to enter day name , What did you enter?");
 	      }
-	      
-
-	      
-	      //but we use else and java message "hey user..."
-	      //so we use else if
-	      //if the 1. conditional is true executed that code and then java will not 2. conditional ==> else if
-	      
-	  /*    if(dayName.equals("Monday") || dayName.equals("Tuesday") || dayName.equals("Wednesday")||dayName.equals("Thursday") || dayName.equals("Friday"))      
-	          // "||" one of them true so conditional is true 
-	            {
-	            System.out.println("Week day");	
-	             }
-	   	     else if(dayName.equals("Saturday") || dayName.equals("Sunday"))
-	   	      {
-	   	    	  System.out.println("Weekend day");
-	   	      }
-	   	      else {
-	    	  System.out.println("Hey user, I told you to enter day name , What did you enter?");
-	      }
-	      */
+	
 	      scan.close();
 	}
 

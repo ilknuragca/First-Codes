0aaa39fbb072014894b61f57e5cfdcd03579b271
ilknuragca <93740795+ilknuragca@users.noreply.github.com>
2021-12-02 22:47:51 +0300

    Update IfStatement01.java

--- IfStatement01.java
+++ IfStatement01.java
@@ -4,14 +4,7 @@ import java.util.Scanner;
 
 public class IfStatement01 {
 	
-  /*    
-       "If it rains , I will cancel the picnic." in English
-       "If (it rains) { I will cancel the picnic}" in java, all keyword are in lowercase.(package,public,class, static, void)
-                                                            ýf is keyword. so if is lowercase
-                                                            
-    Note: the code to check if a number is even or not ==> number%2==0      = assignment operator    == equal                                             
   
-  */
 	public static void main(String[] args) {
 		
 		Scanner scan=new Scanner(System.in);
@@ -22,8 +15,7 @@ public class IfStatement01 {
 		
 		// 1. Example: Get a number from user . If the number is even, print "Even" on the console.	
 		
-		if(num%2==0)        //Note: even numbers are divisible by 2.  when you divide a number by 2, remainder will be 0. 
-			               // so ýf you need to check remainder . you have to use modules operator.
+		if(num%2==0)        
 		{
 			System.out.println( num + " is Even");
 		}
@@ -36,7 +28,7 @@ public class IfStatement01 {
         	System.out.println(num+ " is Odd");
         }
 		
-		//3.Example: Get 2 number from user. ýf they equal to each other , print "Square" on the console
+		//3.Example: Get 2 number from user. Ã½f they equal to each other , print "Square" on the console
         
         if(num == num2)     //equal is ==
         

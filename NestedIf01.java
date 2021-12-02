ef0eb2db9c3bc07e13c8d48b1cc85d95b55a0bd4
ilknuragca <93740795+ilknuragca@users.noreply.github.com>
2021-12-02 23:07:03 +0300

    Update NestedIf01.java

--- NestedIf01.java
+++ NestedIf01.java
@@ -16,23 +16,16 @@ public class NestedIf01 {
 	     otherwise the output will be "Invalid Password"
 	     For ex; zoe==> Valid Password   -  Zoe==>Invalid   -  John ==> Invalid
 	 */
-/*
- * Note: Every character in a string has index. 
- *        Indexes start from zero so the index of first character is zero.
- *        Index of last character in a String equals "the number of characters - 1"
- */
+
 	public static void main(String[] args) {
 	Scanner scan=new Scanner(System.in);
 	System.out.println("Enter the password ");
 	String pwd=scan.next();
 	 
-	char firstName=pwd.charAt(0); /// tüm pwd.charAt(0) yerine firstName yaz. böylece her seferinde java method çalýþtýrmaz
-	                              // böyle çalýþmasý daha hýzlý olur
-	                          //methods will work just one . java just one will go to the String at get character
-	if(pwd.charAt(0)>='A' && pwd.charAt(0)<='Z') //indexes start from zero----charAt(0)==>                        
-		                                         //look at the 1.character from index. charAt(1)==>2.character    
+	char firstName=pwd.charAt(0); 
+	if(pwd.charAt(0)>='A' && pwd.charAt(0)<='Z')    
 	{                                                                                                             
-		if(pwd.charAt(0)=='A')                                            //is the "Nested if" statement                                             
+		if(pwd.charAt(0)=='A')                                                                                      
 		{
 			System.out.println(pwd + " is a valid password");
 		} 
